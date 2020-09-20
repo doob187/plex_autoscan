7137e06febef146ed9bd966f2257691899f744c9
doob187 <60312740+doob187@users.noreply.github.com>
2020-09-20 20:05:00 +0200

    Update test_threads.py

--- test_threads.py
+++ test_threads.py
@@ -3,20 +3,17 @@ import time
 
 from threads import Thread, PriorityLock
 
-
-############################################################
+###########################################################
 # MISC
 ############################################################
-
 def test_thread(lock, priority):
     lock.acquire(priority)
     try:
-        print("Hello from priority: %s" % str(priority))
+        print(("Hello from priority: %s" % str(priority)))
         time.sleep(5)
     finally:
         lock.release()
-    print("Finished priority: %s" % str(priority))
-
+    print(("Finished priority: %s" % str(priority)))
 
 ############################################################
 # MAIN
@@ -25,11 +22,11 @@ def test_thread(lock, priority):
 if __name__ == "__main__":
     lock = PriorityLock()
     threads = Thread()
-    for pos in reversed(range(0, 100)):
+    for pos in reversed(list(range(0, 100))):
         threads.start(test_thread, args=[lock, pos], track=True)
     print("Started first batch of threads")
     time.sleep(15)
-    for pos in reversed(range(0, 100)):
+    for pos in reversed(list(range(0, 100))):
         threads.start(test_thread, args=[lock, pos], track=True)
     print("Started second batch of threads")
     threads.join()
